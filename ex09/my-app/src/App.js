be248ebfeba722101e8947fd96f073aea85d4651
mhkim0240 <dev.mhkim0240@gmail.com>
2024-01-03 23:07:22 +0900

    Unit~30

---
+++ ex09/my-app/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Card from './components/Card';
+
+function App() {
+  return (
+    <div><h1>Pruduct List Page</h1>
+    <hr/>
+    <div>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+    </div>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ ex09/my-app/src/components/Card.js
@@ -0,0 +1,12 @@
+import '../App.css';
+
+function Card() {
+  return (
+    <div className='product-card'>
+        Product1
+    </div>
+
+  );
+}
+
+export default Card;
